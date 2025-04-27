fdb6d2d4f15a4ff05b5d39d4fe9734107b2042a8
Erik Young <young.erik9@gmail.com>
2025-04-27 14:43:23 -0700

    dynamic tab config changes

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -3,7 +3,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
-  Input,
   OnDestroy,
   OnInit,
   viewChild,
@@ -15,10 +14,12 @@ import {
   IonTabButton,
   IonTabs,
 } from '@ionic/angular/standalone';
-import { Subject, takeUntil } from 'rxjs';
+import { LetDirective } from '@ngrx/component';
+import { Store } from '@ngrx/store';
+import { map, Observable, Subject, takeUntil } from 'rxjs';
 import { TabRoutes } from 'src/app/app.routes';
-import { TeamDetail } from 'src/app/core/models/TeamDetail.model';
 import { TabsService } from 'src/app/core/services/tabs/tabs.service';
+import { teamsFeature } from 'src/app/core/store/teams/feature/teams.feature';
 
 type TabConfig = {
   label: string;
@@ -31,32 +32,44 @@ type TabConfig = {
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [IonLabel, IonIcon, IonTabButton, IonTabBar, IonTabs, NgFor, NgIf],
+  imports: [
+    IonLabel,
+    IonIcon,
+    IonTabButton,
+    IonTabBar,
+    IonTabs,
+    NgFor,
+    NgIf,
+    LetDirective,
+  ],
 })
 export class TabsPage implements OnInit, OnDestroy {
   private readonly unsubscribe$ = new Subject<void>();
+  private readonly store = inject(Store);
   private readonly tabsService = inject(TabsService);
 
   private readonly tabsComponent = viewChild(IonTabs);
 
-  @Input() set favoriteTeam(teamDetail: TeamDetail | undefined) {
-    this.myTeamTab =
-      teamDetail !== undefined
-        ? {
-            label: teamDetail.school.abbreviation ?? 'My Team',
-            iconSrc: teamDetail.school.logoUrl,
-            route: TabRoutes.MyTeam,
-          }
-        : {
-            label: 'My Team',
-            icon: 'home-sharp',
-            route: TabRoutes.MyTeam,
-          };
-  }
+  protected readonly myTeamTab$: Observable<TabConfig> = this.store
+    .select(teamsFeature.selectFavoriteTeam)
+    .pipe(
+      map((favoriteTeamAsync) => {
+        const favoriteTeam = favoriteTeamAsync?.data;
+        return favoriteTeam !== undefined
+          ? {
+              label: favoriteTeam.school.abbreviation ?? 'My Team',
+              iconSrc: favoriteTeam.school.logoUrl,
+              route: TabRoutes.MyTeam,
+            }
+          : {
+              label: 'My Team',
+              icon: 'home-sharp',
+              route: TabRoutes.MyTeam,
+            };
+      })
+    );
 
-  myTeamTab!: TabConfig;
-
-  tabs: TabConfig[] = [
+  protected readonly tabs: TabConfig[] = [
     {
       label: 'Search',
       icon: 'search-sharp',
